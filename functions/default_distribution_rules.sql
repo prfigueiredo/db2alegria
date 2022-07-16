bef810a1c5f667cca151a4e8869edaae5ee502ca
jackson.candido <jackson.candido@g.globo>
2022-07-16 15:30:48 -0300

    Adiciona funcao [4] que associa regra de distribuicao default

---
+++ functions/default_distribution_rules.sql
@@ -0,0 +1,24 @@
+-- DROP FUNCTION IF EXISTS setDefaultDistributionRule;
+
+CREATE OR REPLACE FUNCTION setDefaultDistributionRule(videoID INTEGER) RETURNS VOID AS
+$$
+DECLARE
+   brasilID INTEGER;
+  validateRules INTEGER;
+BEGIN
+   SELECT country_id INTO brasilID FROM movie_db.countries WHERE top_level_domain = 'BR';
+   SELECT count(*) INTO validateRules FROM movie_db.distribution_rules WHERE country_id = brasilID AND video_id = videoID;
+
+   IF validateRules = 0 THEN
+	 INSERT INTO movie_db.distribution_rules (country_id, video_id) VALUES (brasilID, videoID);
+   END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+
+-- TESTING
+-- INSERT INTO movie_db.countries (top_level_domain) VALUES ('BR');
+-- INSERT INTO movie_db.videos (duration, published_status, views, title) VALUES (200, TRUE, 0, 'HISTÓRIA SEM FIM');
+-- SELECT setDefaultDistributionRule(<video_id>);
+-- SELECT * FROM movie_db.distribution_rules;
+612a15af545cff03d278b30a5ffe29d72a0b0719
