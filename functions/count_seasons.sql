10a875085a1c25b79449b239ba68e88a0aa4e28e
jackson.candido <jackson.candido@g.globo>
2022-07-17 20:27:40 -0300

    Inclui view de videos publicados

--- functions/count_seasons.sql
+++ functions/count_seasons.sql
@@ -9,9 +9,9 @@ END;
 $$ LANGUAGE plpgsql;
 
 -- TESTING countEpsodiesBySeason
-INSERT INTO movie_db.synopsis (description) VALUES ('synopsis');
-INSERT INTO movie_db.seasons (synopsis_id, description, qty_episodes) VALUES (1, 'description', 0);
-INSERT INTO movie_db.videos (duration, published_status, title) VALUES (200, TRUE, 'HISTÓRIA SEM FIM');
-INSERT INTO movie_db.series (synopsis_id, description) VALUES (1, 'description');
-INSERT INTO movie_db.episodes (name, duration, season_id, serie_id, video_id) VALUES ('nome do episodio', 1440, 1, 1, 1);
-SELECT movie_db.countSeasonBySerie(1);
+-- INSERT INTO movie_db.synopsis (description) VALUES ('synopsis');
+-- INSERT INTO movie_db.seasons (synopsis_id, description, qty_episodes) VALUES (1, 'description', 0);
+-- INSERT INTO movie_db.videos (duration, published_status, title) VALUES (200, TRUE, 'HISTÓRIA SEM FIM');
+-- INSERT INTO movie_db.series (synopsis_id, description) VALUES (1, 'description');
+-- INSERT INTO movie_db.episodes (name, duration, season_id, serie_id, video_id) VALUES ('nome do episodio', 1440, 1, 1, 1);
+-- SELECT movie_db.countSeasonBySerie(1);
