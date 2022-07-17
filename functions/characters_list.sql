46740c131cd64e2d82075ce70cec7b055eb9730d
jackson.candido <jackson.candido@g.globo>
2022-07-17 18:10:05 -0300

    Atualiza funcao de busca de personagens por video_id

--- functions/characters_list.sql
+++ functions/characters_list.sql
@@ -1,13 +1,13 @@
-CREATE OR REPLACE FUNCTION movie_db.characterListByMovieID(movieID INTEGER) RETURNS TABLE (CHARACTER_NAME VARCHAR) AS
+CREATE OR REPLACE FUNCTION movie_db.characterListInMovie(videoID INTEGER) RETURNS TABLE (CHARACTER_NAME VARCHAR) AS
 $$
 BEGIN
     RETURN QUERY SELECT characters.name
             FROM movie_db.movies INNER JOIN movie_db.characters ON movie_db.characters.movie_id = movie_db.movies.movie_id
-            WHERE movie_db.movies.video_id = movieID;
+            WHERE movie_db.movies.video_id = videoID;
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION movie_db.characterListByEpsodieID(videoID INTEGER) RETURNS TABLE (CHARACTER_NAME VARCHAR) AS
+CREATE OR REPLACE FUNCTION movie_db.characterListInEpsodie(videoID INTEGER) RETURNS TABLE (CHARACTER_NAME VARCHAR) AS
 $$
 BEGIN
     RETURN QUERY SELECT characters.name
@@ -18,14 +18,33 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+CREATE OR REPLACE FUNCTION movie_db.characterListByVideoID(videoID INTEGER) RETURNS TABLE (CHARACTER_NAME VARCHAR) AS
+$$
+DECLARE
+    checkMovie INTEGER;
+BEGIN
+    SELECT count(movie_id) INTO checkMovie FROM  movie_db.movies WHERE movies.video_id = videoID;
+
+    IF checkMovie <> 0 THEN
+        RETURN QUERY SELECT movie_db.characterListInMovie(videoID);
+    END IF;
+
+
+    RETURN QUERY SELECT movie_db.characterListInEpsodie(videoID);
+END;
+$$ LANGUAGE plpgsql;
+
 -- -- TESTING
 -- INSERT INTO movie_db.synopsis (description) VALUES ('synopsis');
 -- INSERT INTO movie_db.seasons (synopsis_id, description, qty_episodes) VALUES (1, 'description', 0);
 -- INSERT INTO movie_db.series (synopsis_id, description) VALUES (1, 'description');
 -- INSERT INTO movie_db.videos (duration, published_status, title) VALUES (200, TRUE, 'HISTÓRIA SEM FIM');
 -- INSERT INTO movie_db.genres (name, description) VALUES ('HORROR', 'TRUE HORROR');
--- INSERT INTO movie_db.episodes (name, duration, season_id, serie_id, video_id) VALUES ('nome do episodio', 1440, 1, 1, 1);
--- INSERT INTO movie_db.characters (name, episode_id, movie_id) VALUES ('J. Doe', 1, 1);
 -- INSERT INTO movie_db.movies (video_id,synopsis_id,genre_id, title) VALUES (1, 1, 1, 'HISTÓRIA SEM FIM');
+-- INSERT INTO movie_db.episodes (name, duration, season_id, serie_id, video_id) VALUES ('nome do episodio', 1440, 1, 1, 1);
+-- INSERT INTO movie_db.characters (name, episode_id) VALUES ('J. Doe', 1);
+-- INSERT INTO movie_db.characters (name, movie_id) VALUES ('J. Doe MOVIE', 1);
+
 -- SELECT movie_db.characterListByEpsodieID(1);
 -- SELECT movie_db.characterListByMovieID(1);
+-- SELECT movie_db.characterListByVideoID(1);
