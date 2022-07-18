6ae0a98427c845ee22a0f7bd57ed06ccca6f8a5e
jackson.candido <jackson.candido@g.globo>
2022-07-17 22:42:04 -0300

    Inclui imagem de exemplo da consulta de id de videos HD

--- selects/videos/hd_videos.sql
+++ selects/videos/hd_videos.sql
@@ -1,17 +1,17 @@
 
-INSERT INTO movie_db.videos (duration, published_status, title) VALUES (200, TRUE, 'HISTÓRIA SEM FIM');
-INSERT INTO movie_db.countries (top_level_domain) VALUES ('BR');
-INSERT INTO movie_db.languages (name, country_id) VALUES ('LINGUA', 1);
-INSERT INTO movie_db.audios (language_id, name) VALUES (1, 'portuguesa');
-INSERT INTO movie_db.subtitles (language_id, name) VALUES (1, 'PT [BR]');
-INSERT INTO movie_db.tech_specs (video_width, video_height, video_quality, audio_quality, dolby_surround) VALUES (1280, 720, 'HD', 'HD', TRUE);
-INSERT INTO movie_db.tech_specs (video_width, video_height, video_quality, audio_quality, dolby_surround) VALUES (1920, 1080, 'HD', 'HD', TRUE);
-INSERT INTO movie_db.tech_specs (video_width, video_height, video_quality, audio_quality, dolby_surround) VALUES (640, 360, 'LD', 'LD', TRUE);
-INSERT INTO movie_db.playback_config (tech_spec_id, audio_id, subtitile_id, video_id) VALUES (1,1,1,1);
-INSERT INTO movie_db.playback_config (tech_spec_id, audio_id, subtitile_id, video_id) VALUES (2,1,1,1);
-INSERT INTO movie_db.playback_config (tech_spec_id, audio_id, subtitile_id, video_id) VALUES (2,1,1,1);
+-- INSERT INTO movie_db.videos (duration, published_status, title) VALUES (200, TRUE, 'HISTÓRIA SEM FIM');
+-- INSERT INTO movie_db.countries (top_level_domain) VALUES ('BR');
+-- INSERT INTO movie_db.languages (name, country_id) VALUES ('LINGUA', 1);
+-- INSERT INTO movie_db.audios (language_id, name) VALUES (1, 'portuguesa');
+-- INSERT INTO movie_db.subtitles (language_id, name) VALUES (1, 'PT [BR]');
+-- INSERT INTO movie_db.tech_specs (video_width, video_height, video_quality, audio_quality, dolby_surround) VALUES (1280, 720, 'HD', 'HD', TRUE);
+-- INSERT INTO movie_db.tech_specs (video_width, video_height, video_quality, audio_quality, dolby_surround) VALUES (1920, 1080, 'HD', 'HD', TRUE);
+-- INSERT INTO movie_db.tech_specs (video_width, video_height, video_quality, audio_quality, dolby_surround) VALUES (640, 360, 'LD', 'LD', TRUE);
+-- INSERT INTO movie_db.playback_config (tech_spec_id, audio_id, subtitile_id, video_id) VALUES (1,1,1,1);
+-- INSERT INTO movie_db.playback_config (tech_spec_id, audio_id, subtitile_id, video_id) VALUES (2,1,1,1);
+-- INSERT INTO movie_db.playback_config (tech_spec_id, audio_id, subtitile_id, video_id) VALUES (2,1,1,1);
 
-SELECT MOVIE_DB.videos.video_id
+SELECT MOVIE_DB.videos.video_id AS "Indentidicador de Vídeo c/ playback HD"
 FROM MOVIE_DB.videos
 INNER JOIN MOVIE_DB.playback_config
     ON MOVIE_DB.playback_config.video_id = MOVIE_DB.videos.video_id
